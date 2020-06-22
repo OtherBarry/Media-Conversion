d54dbe9538eb9d889a7dc768e550b846c1eb71a0
OtherBarry <charlie.wilson4@gmail.com>
2020-06-22 12:15:17 +1000

    Add Radarr support

--- webserver.py
+++ webserver.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 from videos import Video
 import datetime
+import requests
 import json
+import os
 
 app = Flask(__name__)
 
@@ -10,10 +12,32 @@ app = Flask(__name__)
 def radarr():
     data = json.loads(request.data)
     if data["eventType"] == "Download":
-        id = data["movieFile"]["id"]
-        relative_path = data["movieFile"]["relativePath"]
-        path = data["movieFile"]["path"]
-        print("{}. {} ({})".format(id, path, relative_path))
+        log("\nDownload received from Radarr at {}".format(datetime.datetime.now()))
+        id = data["movie"]["id"]
+        request = requests.get("http://192.168.0.10:7878/api/movie/{}".format(id),
+                                params={"apikey": "26c68f7dfb6d4ad481a33e32a4bf1579"})
+        data = request.json()
+        if not data["downloaded"]:
+            log("\tAPI Error: Movie not labelled 'downloaded'.")
+            return "OK"
+        path = data["path"] + "/" + data["movieFile"]["relativePath"]
+        path = path.replace("/data/media/", "M:/")
+        log("\tFile: " + path)
+        vid = Video(path, "movie")
+        log("\tCodec: {}\n\tWidth: {}\n\tBitrate: {}".format(vid.codec,
+                                                             vid.width,
+                                                             vid.rate))
+        if vid.needs_transcoding:
+            params = vid.params.copy()
+            params.pop("i")
+            log("\tParams: {}".format(params))
+            if vid.transcode():
+                log("\tSuccessfully Transcoded")
+            else:
+                log("\tTranscode Failed")
+            log("\tTranscode ended at {}".format(datetime.datetime.now()))
+        else:
+            log("\tNo Transcode Required")
     elif data["eventType"] == "Test":
         print("This is a test")
     return "OK"
@@ -23,7 +47,7 @@ def radarr():
 def sonarr():
     data = json.loads(request.data)
     if data["eventType"] == "Download":
-        log("\nDownload received at {}".format(datetime.datetime.now()))
+        log("\nDownload received from Sonarr at {}".format(datetime.datetime.now()))
         folder = data["series"]["path"]
         file = data["episodeFile"]["relativePath"]
         path = folder + "/" + file
@@ -59,5 +83,5 @@ def log(line):
 
 
 if __name__ == '__main__':
-    log("Converter Started at {}".format(datetime.datetime.now()))
+    log("Webserver Started at {}".format(datetime.datetime.now()))
     app.run()
