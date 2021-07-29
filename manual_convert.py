6ebb007868c9647395755c500ecc52c00b1d3a8a
Charlie Wilson <charlie.wilson4@gmail.com>
2021-07-29 11:51:10 +1000

    Update manual_convert to use rq for transcoding

--- manual_convert.py
+++ manual_convert.py
@@ -1,38 +1,32 @@
-import os
-import glob
-from videos import Video
 import datetime
+import glob
+import os
+
+from videos import Video, transcode_file
+from redis import Redis
+from rq import Queue
+
+
+MEDIA_LIBRARIES = {
+    "tv": "/data/media/TV Shows/",
+    "movie": "/data/media/Movies/",
+    "animation": "/data/media/Animated TV Shows/",
+}
+TYPES = ["**/*." + x for x in Video.FILE_EXTENSIONS]
 
 
 def log(line):
-    log_file = "logs/{} Log.txt".format(datetime.date.today())
+    log_file = "logs/{}_bulk_transcode.txt".format(datetime.date.today())
     print(line)
     with open(log_file, "a", encoding="utf8") as f:
         f.write(line + "\n")
 
 
-log("Converter Started at {}".format(datetime.datetime.now()))
-media_libraries = {
-    "tv": "M:/TV Shows/",
-    "movie": "M:/Movies/",
-    "animation": "M:/Animated TV Shows/",
-}
-types = ["**/*." + x for x in Video.FILE_EXTENSIONS]
-for type in types:
-    for media_type, media_dir in media_libraries.items():
-        for file in glob.glob(media_dir + type, recursive=True):
+log("Converter Started at {}".format(datetime.datetime.now().strftime("%H:%M:%S")))
+q = Queue(connection=Redis())
+for file_type in TYPES:
+    for media_type, media_dir in MEDIA_LIBRARIES.items():
+        for file in glob.glob(media_dir + file_type, recursive=True):
             file = os.path.abspath(file)
-            log(file)
-            vid = Video(file, media_type)
-            log("\tCodec: {}\n\tWidth: {}\n\tBitrate: {}".format(vid.codec,
-                                                                 vid.width,
-                                                                 vid.rate))
-            if vid.needs_transcoding:
-                log("\tParams: {}".format(vid.params))
-                if vid.transcode():
-                    log("\tSuccessfully Transcoded")
-                else:
-                    log("\tTranscode Failed")
-            else:
-                log("\tNo Transcode Required")
-            log("\n")
+            log(f"Queued for transcoding: {file}")
+            q.enqueue(transcode_file, file, media_type)
