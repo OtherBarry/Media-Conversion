83bac9612d5db9c539f8b658e1f7595078c64326
Charlie Wilson <charlie.wilson4@gmail.com>
2026-03-04 22:19:36 +1100

    feat: Switch to rotating log handling

--- transcoder/logging.py
+++ transcoder/logging.py
@@ -1,12 +1,19 @@
 import logging
+import sys
 from logging.handlers import WatchedFileHandler
 
 from transcoder.settings import settings
 
 
-def configure_logging():
-    log_handler = WatchedFileHandler(settings.log_path)
-    formatter = logging.Formatter(
-        "%(asctime)s - [%(levelname)s] %(message)s", "%b %d %H:%M:%S"
+def configure_logging() -> None:
+    stream_handler = logging.StreamHandler(sys.stdout)
+    file_handler = logging.handlers.TimedRotatingFileHandler(
+        settings.log_path,
+        when="D",
+        interval=1,
+        backupCount=7,
     )
-    log_handler.setFormatter(formatter)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
+    logging.basicConfig(level=logging.DEBUG, handlers=[file_handler, stream_handler])
